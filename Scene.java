c8e1016ddf09ebc9874bd8a578066d459b227082
Akshay Mysore <Akku41809@gmail.com>
2024-06-10 18:51:42 -0400

    added the Scene class but still needs generalization

--- Scene.java
+++ Scene.java
@@ -1,2 +1,27 @@
+import Objects.Shapes.Sphere;
+import java.util.ArrayList;
+
 public class Scene {
+    private ArrayList<Sphere> objects = new ArrayList<>();
+    private Camera camera;
+
+    public Scene(Camera camera){
+        this.camera = camera;
+    }
+
+    public void addObject(Sphere item){
+        objects.add(item);
+    }
+
+    public void setCamera(Camera camera){
+        this.camera = camera;
+    }
+
+    public Camera getCamera(){
+        return this.camera;
+    }
+
+    public ArrayList<Sphere> getObjects(){
+        return objects;
+    }
 }
